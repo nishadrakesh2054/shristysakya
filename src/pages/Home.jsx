4b941d1c62b0cff77abf3e51c48f8995a3937ff6
Rakesh Kumar Sahani <nishadrakesh2054@gmail.com>
2025-11-12 12:29:09 +0545

    bug solved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 // import Brands from "../components/Brands";
-import Projects from "../components/Projects";
+// import Projects from "../components/Projects";
 import Testimonial from "../components/Testimonial";
 import Hero from "../components/Hero";
 import About from "../components/About";
-import Service from "../components/Service";
+// import Service from "../components/Service";
 import HomePagdData from "../data/HomePagdData.json";
 import Experience from "../components/Experience";
 import Contact from "../components/Contact";
@@ -27,11 +27,11 @@ export default function Home() {
   return (
     <>
       <Hero data={hero} socialData={socialBtns} />
-        <About data={about} />
+      <About data={about} />
       <Work data={work} />
       {/* <Brands data={brands} /> */}
       <Newservice data={newservice} />
-    
+
       {/* <Projects data={projects} /> */}
       {/* <Service data={service} /> */}
       <Experience data={experience} />
